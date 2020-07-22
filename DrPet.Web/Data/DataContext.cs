346dc71e0c69a94051695f93b7447db54353fc66
Diogo Silva <diogo.lopo.silva@formandos.cinel.pt>
2020-07-22 10:33:02 +0100

    Create database and animals controller

---
+++ DrPet.Web/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using DrPet.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrPet.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Animal> Animals { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ DrPet.Web/Data/Entities/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DrPet.Web.Data.Entities
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Sex { get; set; }
+
+        public string Species { get; set; }
+
+        public string Breed { get; set; }
+
+        public string Color { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
